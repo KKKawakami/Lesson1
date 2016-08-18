d6a34d9a1a97682a0f2d6a799ee8dfce1f5dafd1
KKKawakmi <kkawakami@yandex.ru>
2016-08-18 13:09:32 +0300

    Add comments

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -8,11 +8,19 @@ namespace Lesson1
 {
     class Program
     {
+        ///<summary>
+        /// Метод Main() является
+        /// входной точкой работы программы
+        /// </summary>
+        /// <param name="args">Аргумент метода Main()</param>
         static void Main(string[] args)
         {
+            //Основная программа
             Console.WriteLine("Hello World!");
             string varrrrior = Console.ReadLine();
             Console.WriteLine(varrrrior);
+
+            //Ожидание нажатия клавиши Enter перед завершением работы
             Console.ReadLine();
         }
     }
